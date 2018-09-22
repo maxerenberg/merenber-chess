0bcc9f6e201de78bbe07bb959c04888f63e33445
Max Erenberg <max.erenberg@outlook.com>
2018-09-22 01:16:42 -0400

    changed clearTables.php to delete all tables

--- web/clearTables.php
+++ web/clearTables.php
@@ -5,6 +5,10 @@ $conn = new PDO(sprintf("pgsql:host=%s;port=%s;user=%s;password=%s;dbname=%s",
 );
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $days = $conn->query("SELECT EXTRACT(DAY FROM (NOW() - STAMP)) FROM RECENTMOVE", PDO::FETCH_NUM)->fetch();
-if ($days !== false and intval($days[0]) > 0) $conn->exec("DELETE FROM RECENTMOVE");
+if ($days !== false and intval($days[0]) > 0) {
+	$conn->exec("DELETE FROM CHESSPLAYERS");
+	$conn->exec("DELETE FROM CHESSBOARD");
+	$conn->exec("DELETE FROM RECENTMOVE");
+}
 $conn = null;
 ?>
